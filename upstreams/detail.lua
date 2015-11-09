b62014e0e53724d53ed5229b1ace9f0c8d741ac3
CMGS <ilskdw@gmail.com>
2015-11-09 15:14:53 +0800

    output upstream details by json

--- upstreams/detail.lua
+++ upstreams/detail.lua
@@ -4,28 +4,13 @@ local get_servers = upstream.get_servers
 local get_upstreams = upstream.get_upstreams
 
 local us = get_upstreams()
+local result = {}
 for _, u in ipairs(us) do
-    ngx.say("upstream ", u, ":")
     local srvs, err = get_servers(u)
     if not srvs then
         ngx.say("failed to get servers in upstream ", u)
     else
-        for _, srv in ipairs(srvs) do
-            local first = true
-            for k, v in pairs(srv) do
-                if first then
-                    first = false
-                    ngx.print("    ")
-                else
-                    ngx.print(", ")
-                end
-                if type(v) == "table" then
-                    ngx.print(k, " = {", concat(v, ", "), "}")
-                else
-                    ngx.print(k, " = ", v)
-                end
-            end
-            ngx.print("\n")
-        end
+        result[u] = srvs
     end
 end
+ngx.say(cjson.encode(result))
